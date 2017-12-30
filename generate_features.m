2fa7868c31cd2826d963142a2e6cfbcb137cb29c
Louis Clergue <louisclergue@gmail.com>
2017-12-30 11:26:43 +0100

    Revert "changed generate_features so that it is possible to add multiple features from one function"
    This reverts commit 11d2396f4ecea06fa3c46678ede863a6f18feff1.

--- generate_features.m
+++ generate_features.m
@@ -3,17 +3,13 @@ function [features] = generate_features(im)
 %   param im:           image to describe
 %   return features:    struct of features
     
-    fs = {@std2, @mean2, @Hog}; % stupid test features
+    fs = {@std2, @mean2}; % stupid test features
     features = struct();
     
     for i = 1:size(fs,2)
         f = fs{i};
         finfo = functions(f);
-        funresult = f(im);
-        for j=1:size(funresult,2)
-            name = strcat(finfo.function,int2str(j));
-            features.(name) = funresult(j);
-        end
+        features.(finfo.function) = f(im);
     end
 end
 
