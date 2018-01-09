15ef2da1906cc69f009a99963e8769d76835420b
unknown <laiafreixas9@gmail.com>
2018-01-09 15:12:16 +0100

    added functions to predict if looking to camera

--- main.m
+++ main.m
@@ -27,9 +27,9 @@ model = TreeBagger(100,training_data,'class');
 % Use ResponseVarName to specify label 'class'
 
 % test model: precision, recall, accuracy
-testing_noclass = testing_data;
-testing_noclass.class = []; % remove class column
-prediction = predict(model, testing_noclass);
+testing_nolabel = testing_data;
+testing_nolabel.class = []; % remove class column
+prediction = predict(model, testing_nolabel);
 
 conf_matrix = eval_prediction(prediction, testing_data.class)
 
@@ -44,3 +44,25 @@ conf_matrix = eval_prediction(prediction, testing_data.class)
 %Lluminositat normalitzada
 %Classificador
 %Descriptors
+
+%Dades per deteccio de mirada
+lookingV = get_looking_vector(data);
+eye_data.looking = lookingV';
+
+%Separar ulls mirant i no-mirant
+eye_looking = eye_data(eye_data.looking==1,:);
+no_eye_looking = eye_data(eye_data.looking==0,:);
+
+
+% separate training, testing data
+[training_data_look, testing_data_look] = split_data(eye_looking, no_eye_looking);
+
+%train model
+modelLook = TreeBagger(100,training_data_look,'looking');
+
+testing_nolabel = testing_data_look;
+testing_nolabel.looking = [];
+predictionLook = predict(modelLook, testing_nolabel);
+
+looking_labels = cellstr(num2str(testing_data_look.looking));
+conf_matrix_look = eval_prediction (predictionLook, looking_labels)
